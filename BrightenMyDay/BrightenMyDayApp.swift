e91e4d31608b8e9cccdf631d126b2cec4a32016e
vincemuller <vmuller2529@gmail.com>
2024-09-05 08:51:02 -0700

    Initial Commit

---
+++ BrightenMyDay/BrightenMyDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrightenMyDayApp.swift
+//  BrightenMyDay
+//
+//  Created by Vince Muller on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BrightenMyDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
